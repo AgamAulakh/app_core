af7d12e409080ddf406528325d952396eb2e6257
mkenzie-m <mackenzie.a.morris@gmail.com>
2024-01-19 23:27:53 -0700

    Add BLE Support
    Add BLE support - not tested

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,12 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/init.h>
+#include <zephyr/smf.h>
+
+extern "C" {
+#include <ble_handler.h>
+}
+
 #include <nrf.h>
 #include <nrfx.h>
 
@@ -94,6 +100,32 @@ static void test_arm_rfft_f32_real(const uint32_t *input, const uint32_t *ref, s
 	test_arm_rfft_f32_real_backend(false, input, ref, length);
 }
 
+/** @brief Allow access to specific GPIOs for the network core.
+ *
+ * Function is executed very early during system initialization to make sure
+ * that the network core is not started yet. More pins can be added if the
+ * network core needs them.
+ */
+static int network_gpio_allow(void)
+{
+	// TBD
+	// uint32_t start_pin = (IS_ENABLED(CONFIG_SOC_ENABLE_LFXO) ? 2 : 0);
+
+	// /* Allow the network core to use all GPIOs. */
+	// for (uint32_t i = start_pin; i < P0_PIN_NUM; i++) {
+	// 	NRF_P0_S->PIN_CNF[i] = (GPIO_PIN_CNF_MCUSEL_NetworkMCU <<
+	// 				GPIO_PIN_CNF_MCUSEL_Pos);
+	// }
+
+	// for (uint32_t i = 0; i < P1_PIN_NUM; i++) {
+	// 	NRF_P1_S->PIN_CNF[i] = (GPIO_PIN_CNF_MCUSEL_NetworkMCU <<
+	// 				GPIO_PIN_CNF_MCUSEL_Pos);
+	// }
+
+
+	return 0;
+}
+
 int main(void)
 {
 	/* Power off RAM and suspend CPU */
@@ -108,6 +140,13 @@ int main(void)
 	uint32_t fft_output[fft_length] = { 0 };
 
 	test_arm_rfft_f32_real(fft_input, fft_output, fft_length);
+
+	init_ble_handler();
+
+	while(1) {
+		LOG_DBG("main thread up time: %u ms", k_uptime_get_32());
+		k_msleep(LOG_DELAY_MS);
+	}
 
 	return 0;
 }
@@ -144,3 +183,7 @@ static int network_gpio_allow(void)
 }
 
 SYS_INIT(network_gpio_allow, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_OBJECTS);
+
+// TBD
+//SYS_INIT(network_gpio_allow, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_OBJECTS);
+
