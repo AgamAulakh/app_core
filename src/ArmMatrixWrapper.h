811b11ac2b718ad9cae75bf3bd23c974600bc96d
liy-hu <lisa.huang1@ucalgary.ca>
2024-02-12 17:33:08 -0700

    Add functions for single-sided FFT and Power calculations

--- src/ArmMatrixWrapper.h
+++ src/ArmMatrixWrapper.h
@@ -210,13 +210,77 @@ public:
         return mseValue;
     };
 
-    // // TEMPORARY!!!
-    // // Fast Fourier Transform Wrapper (FFT)
-    // // Assumes input is real data row vector, and output will be complex row vector
-    // ArmMatrixWrapper<1, MaxCols> fft() const {
-    //     ArmMatrixWrapper<1, MaxCols> result;
-    //     arm_cfft_f32(&arm_cfft_sR_f32_len, reinterpret_cast<float32_t*>(data), 0, 1);
-    //     memcpy(result.data, data, MaxBufferSize * sizeof(float32_t));
-    //     return result;
-    // };
+    // Computes the raw FFT for specific channel given a number of samples 
+    ArmMatrixWrapper<MaxRows, 1> rawFFT(uint32_t channel) const {
+        
+        // Struct to store the output of the FFT for one channel 
+        ArmMatrixWrapper<MaxRows/2, 1> rawResult;
+
+        // Temporary arrays to store initial data and intermediate FFT values
+        float32_t inputFFT[matrix.numRows];
+        float32_t outputFFT[matrix.numRows];
+       
+        // O indicates FFT and 1 indicates inverse FFT (This flag will not change)
+        const uint32_t ifftFlag = 0;
+
+        // Initalize the instance with the specified number of rows
+        arm_rfft_fast_instance_f32 fft_instance;
+        arm_rfft_fast_init_f32(&fft_instance, matrix.numRows);
+        
+        for (uint32_t i = 0; i < matrix.numRows; i++)
+        {
+            inputFFT[i] = at(i, channel);
+        }
+
+        // Compute the FFT and store the outputs of the FFT in the array
+        arm_rfft_fast_f32(&fft_instance, inputFFT , outputFFT, ifftFlag);
+
+        // The first entry is the DC offset so simply set to 0
+        outputFFT[0] =  0;
+
+        // Copy output FFT to result
+        memcpy(rawResult.data, outputFFT, MaxBufferSize * sizeof(float32_t));
+        return rawResult;
+    };
+
+
+    // Computes the single-sided FFT for a specific channel given a number of samples
+    ArmMatrixWrapper<MaxRows/2, 1> singleSideFFT(uint32_t channel) const {
+
+        // Struct to store the output of the FFT for one channel 
+        ArmMatrixWrapper<MaxRows/2, 1> FFTResult;
+        
+        // Temporary arrays to store initial data and intermediate FFT values
+        float32_t outputFFTMag[matrix.numRows/2];
+
+        // Extract the magnitude values because FFT values are complex and have both a magnitude and phase.
+        // matrix.numRows/2 because we are looking at one-sided spectrum (# of magnitudes we want to look at)
+        arm_cmplx_mag_f32(rawFFT(channel).data, outputFFTMag, matrix.numRows/2);
+
+        // Copy the output to the result matrix
+        memcpy(FFTResult.data, outputFFTMag, MaxBufferSize * sizeof(float32_t));
+        return FFTResult;
+
+    }
+
+    // Computes the single-sided Power given the single-sided FFT of a channel
+    ArmMatrixWrapper<MaxRows/2, 1> singleSidePower(uint32_t channel) const {
+        
+        // Struct to store the output of the FFT for one channel 
+        ArmMatrixWrapper<MaxRows/2, 1> PowerResult;
+        
+        // Temporary arrays to store power FFT values
+        float32_t powerFFT[matrix.numRows/2];
+
+        // Compute the power of the FFT
+        arm_cmplx_mag_squared_f32(rawFFT(channel).data, powerFFT, matrix.numRows/2);
+        
+        // Check if the scaling is required for the above function
+        // Copy the output to the result matrix
+        memcpy(PowerResult.data, powerFFT, MaxBufferSize * sizeof(float32_t));
+        return PowerResult;
+
+    }
+
+  
 };
