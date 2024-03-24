1f90f027973eb5591ff32c512c2f56ea4b602625
mkenzie-m <mackenzie.a.morris@gmail.com>
2024-03-24 17:05:24 -0600

    fix file inclusion

--- src/ArmMatrixWrapper.h
+++ src/ArmMatrixWrapper.h
@@ -1,15 +1,32 @@
-#include <stdint.h>
+// #include <stdint.h>
+#include <cstdint>
 #include <cstring>
-#include "arm_math.h"
+#include <vector>
+#include <zephyr/sys/printk.h> 
+
+
+// #include "arm_math/DSP/Include/arm_math.h"
+// #include "v1.9.1/modules/hal/cmsis/CMSIS/DSP/Include/arm_math.h"
+
+//using namespace std;
+//#include "v1.9.1/modules/hal/cmsis/CMSIS/Core/Include/cmsis_compiler.h"
+//#include "../../lisa_test_project/arm_math/DSP/include/arm_math.h"
+// #if defined(LISA_TEST_PROJECT)
+// #include "../../lisa_test_project/arm_math/arm_math.h"
+// #else
+//#include "../../lisa_test_project/arm_math/DSP/Include/arm_math.h"
+// #endif
+#include <arm_math.h>
 
 template <uint32_t MaxRows, uint32_t MaxCols>
 class ArmMatrixWrapper {
 private:
-    static constexpr uint32_t MaxBufferSize = MaxRows * MaxCols;
-    float32_t data[MaxBufferSize];
     arm_matrix_instance_f32 matrix;
 
 public:
+    static constexpr uint32_t MaxBufferSize = MaxRows * MaxCols;
+    float32_t data[MaxBufferSize];
+    
     ArmMatrixWrapper() {
         arm_mat_init_f32(&matrix, MaxRows, MaxCols, data);
     };
@@ -61,6 +78,7 @@ public:
         return matrix.numCols;
     };
 
+
     // Access Functions
     float32_t at(uint32_t index) const {
         if (index < MaxBufferSize) {
@@ -87,6 +105,19 @@ public:
             return 0.0f;
         }
     };
+
+    void prettyPrint() {
+
+        printk("\nmatrix data:\n") ;
+        for (int row = 0; row < matrix.numRows; row++ ) { 
+            for (int col = 0; col < matrix.numCols; col++ ) 
+            { 
+                printk("%.4f\t", this->at(row,col));
+            } 
+        } 
+        printk("\n");
+
+    }
 
     bool set_at(float32_t value, uint32_t i, uint32_t j) {
         if (i < matrix.numRows && j < matrix.numCols) {
@@ -210,13 +241,151 @@ public:
         return mseValue;
     };
 
-    // // TEMPORARY!!!
-    // // Fast Fourier Transform Wrapper (FFT)
-    // // Assumes input is real data row vector, and output will be complex row vector
-    // ArmMatrixWrapper<1, MaxCols> fft() const {
-    //     ArmMatrixWrapper<1, MaxCols> result;
-    //     arm_cfft_f32(&arm_cfft_sR_f32_len, reinterpret_cast<float32_t*>(data), 0, 1);
-    //     memcpy(result.data, data, MaxBufferSize * sizeof(float32_t));
-    //     return result;
-    // };
+    // Computes the raw FFT for specific channel given a number of samples 
+    ArmMatrixWrapper<MaxRows/2, 1> rawFFT(uint32_t channel) const {
+        
+        // Struct to store the output of the FFT for one channel 
+        ArmMatrixWrapper<MaxRows/2, 1> rawResult;
+
+        // Temporary arrays to store initial data and intermediate FFT values
+        float32_t inputFFT[matrix.numRows];
+        float32_t outputFFT[matrix.numRows];
+       
+        // O indicates FFT and 1 indicates inverse FFT (This flag will not change)
+        const uint32_t ifftFlag = 0;
+
+        // Initalize the instance with the specified number of rows
+        arm_rfft_fast_instance_f32 fft_instance;
+        arm_rfft_fast_init_f32(&fft_instance, matrix.numRows);
+        
+        for (uint32_t i = 0; i < matrix.numRows; i++)
+        {
+            inputFFT[i] = at(i, channel);
+        }
+
+        // Compute the FFT and store the outputs of the FFT in the array
+        arm_rfft_fast_f32(&fft_instance, inputFFT , outputFFT, ifftFlag);
+
+        // The first entry is the DC offset so simply set to 0
+        outputFFT[0] =  0;
+
+        // Copy output FFT to result
+        memcpy(rawResult.data, outputFFT, MaxBufferSize * sizeof(float32_t));
+        return rawResult;
+    };
+
+
+    // Computes the single-sided FFT for a specific channel given a number of samples
+    ArmMatrixWrapper<MaxRows/2, 1> singleSideFFT(uint32_t channel) const {
+
+        // Struct to store the output of the FFT for one channel 
+        ArmMatrixWrapper<MaxRows/2, 1> FFTResult;
+        
+        // Temporary arrays to store initial data and intermediate FFT values
+        float32_t outputFFTMag[matrix.numRows/2];
+
+        // Extract the magnitude values because FFT values are complex and have both a magnitude and phase.
+        // matrix.numRows/2 because we are looking at one-sided spectrum (# of magnitudes we want to look at)
+        arm_cmplx_mag_f32(rawFFT(channel).data, outputFFTMag, matrix.numRows/2);
+
+        // Copy the output to the result matrix
+        memcpy(FFTResult.data, outputFFTMag, MaxBufferSize * sizeof(float32_t));
+
+        return FFTResult;
+
+    }
+
+    // Computes the single-sided Power given the single-sided FFT of a channel
+    ArmMatrixWrapper<MaxRows/2, 1> singleSidePower(uint32_t channel) const {
+        
+        // Struct to store the output of the FFT for one channel 
+        ArmMatrixWrapper<MaxRows/2, 1> PowerResult;
+        
+        // Temporary arrays to store power FFT values
+        float32_t powerFFT[matrix.numRows/2];
+
+        // Compute the power of the FFT
+        arm_cmplx_mag_squared_f32(rawFFT(channel).data, powerFFT, matrix.numRows/2);
+        
+        // Check if the scaling is required for the above function
+        // Copy the output to the result matrix
+        memcpy(PowerResult.data, powerFFT, MaxBufferSize * sizeof(float32_t));
+        return PowerResult;
+
+    }
+
+    // Computes the single-sided Band Power given the single-sided FFT of a channel,
+    // Pwelch of specified channel and specific band power range type
+    float32_t singleSideBandPower(uint32_t channel, uint32_t sampleFreq, uint32_t sampleNo, uint32_t bandSelect) const {
+        
+        // The frequency resolution or frequency bin width
+        float32_t freqRes = sampleFreq / sampleNo;
+
+        // The following band limits: delta band, theta band, alpha band, beta band
+        float32_t bandRanges[] = {1,3,4,7,8,12,13,30};
+
+        // Low and high limit for specified band range of frequencies
+        uint32_t lowLimit = 0;
+        uint32_t highLimit = 0;
+
+        switch(bandSelect){
+
+            // delta band (Note: make an enum for the different band types)
+            case 0:{
+                lowLimit = (uint32_t)(floor(bandRanges[0] / freqRes));  
+                highLimit = (uint32_t)(ceil(bandRanges[1] / freqRes));
+                break;
+            } 
+            // theta band
+            case 1:{
+                lowLimit = (uint32_t)(floor(bandRanges[2] / freqRes));
+                highLimit = (uint32_t)(ceil(bandRanges[3] / freqRes));
+                break;
+            }
+            // alpha band
+            case 2:{
+                lowLimit = (uint32_t)(floor(bandRanges[4] / freqRes));
+                highLimit = (uint32_t)(ceil(bandRanges[5] / freqRes));
+                break;
+            }
+            // beta band
+            case 3:{
+                lowLimit = (uint32_t)(floor(bandRanges[6] / freqRes));
+                highLimit = (uint32_t)(ceil(bandRanges[7] / freqRes));
+                break;
+            }
+            default:{
+                break;
+            }
+
+        }
+
+        // Calculating the band power for specified band range of frequnencies
+        float32_t bandPower = 0;
+        for(uint32_t i = lowLimit; i < highLimit; lowLimit++){
+            bandPower += (data[i] * freqRes);
+        }
+        return bandPower;
+    }
+
+    // Computes the single-sided Relative Band Power given the specified bandPower values
+   std::vector<float32_t> singleSideRelativeBandPower(std::vector<float32_t> bandPowers){
+        
+        std::vector<float32_t> relativeBandpowers(4);
+
+        // Calculate summation of total power for one channel
+        float32_t totalPower = 0;
+        for(uint32_t i = 0; i < MaxBufferSize; i++){
+            totalPower += data[i];
+        }
+
+        // Calculate relative band power relative to the total power of one channel
+        for(uint32_t i = 0; i < bandPowers.size(); i++){
+
+            relativeBandpowers[i] = bandPowers[i] / totalPower;
+        }
+
+        return relativeBandpowers;
+    }
+
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,136 @@
-/*
- * Copyright (c) 2020 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
- */
-
-#include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/logging/log.h>
-#include <zephyr/logging/log_ctrl.h>
-#include <zephyr/init.h>
-#include <nrf.h>
-#include <nrfx.h>
-
-#include <stdlib.h>
-
 #include "ArmMatrixWrapper.h"
-#include "DataAcquisitionThread.h"
-#include "drivers/ads1299-x.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include "FFTsignal.h"
 
-// temporary
-#include <zephyr/drivers/pwm.h>
+using namespace std;
 
-LOG_MODULE_REGISTER(eegals_app_core, LOG_LEVEL_DBG);
+#define LISA_TEST_PROJECT 1
+#define RAW_SAMPLE_NUMBER 1024 // Just testing for one channel
+#define PROCESSED_SAMPLE_NUMBER (RAW_SAMPLE_NUMBER/2)
+#define SAMPLE_FREQ 250 
+#define CHANNELS 1 // Just for testing one channel 
+#define BANDS 4
 
-static const struct pwm_dt_spec blue_pwm_led = PWM_DT_SPEC_GET(DT_ALIAS(blueled));
-
-#define LOG_DELAY_MS 5000
-#define LED_PERIOD 1000
-#define LED_OFF 0
-#define LED_CHANNEL 0
-#define LED_BLUE_PULSE_WIDTH 1000
-
-
-int main(void)
-{
-	LOG_INF("Hello world from %s", CONFIG_BOARD);
-
-	DataAcquisitionThread::GetInstance().Initialize();
-
-	while(1) {
-		LOG_DBG("main thread up time: %u ms", k_uptime_get_32());
-		DataAcquisitionThread::GetInstance().SendMessage(
-			DataAcquisitionThread::RESET_AFE
-		);
-		pwm_set_dt(&blue_pwm_led, LED_PERIOD, LED_BLUE_PULSE_WIDTH);
-		k_msleep(LOG_DELAY_MS);
-
-		pwm_set_dt(&blue_pwm_led, LED_PERIOD, LED_OFF);
-		k_msleep(LOG_DELAY_MS);
-	}
-
-	return 0;
+ enum PowerBands : uint8_t {
+        DELTA = 0,
+        THETA,
+        ALPHA,
+        BETA,
 };
+
+int main () {
+    // Read the CSV file 
+    ifstream file("Test_1.csv");
+    
+    if (!file.is_open()) {
+        cerr << "Error opening file!" << endl;
+        return 1;
+    }
+    
+    string line;
+    vector< vector<string> > rawData; // Vector of vectors to store CSV rawData
+    
+    // Read each line from the file
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string item;
+        vector<string> items;
+        
+        // Split the line by commas and store in a vector
+        while (getline(ss, item, ',')) {
+            items.push_back(item);
+        }
+        
+        // Add the vector of items to the rawData vector
+        rawData.push_back(items);
+    }
+    
+    // Close the file
+    file.close();
+    
+    // 256 samples, with 8 channels
+    ArmMatrixWrapper<RAW_SAMPLE_NUMBER, 8> allChannels;
+
+    // Array of FFT results of all 8 channels
+    vector< ArmMatrixWrapper<PROCESSED_SAMPLE_NUMBER, 1> > channelFFTResults(CHANNELS);
+  
+    // Array of Power spectrum of all 8 channels 
+    vector< ArmMatrixWrapper<PROCESSED_SAMPLE_NUMBER, 1> > channelPowerResults(CHANNELS);
+   
+    // Array of channels where each channel has 4 elements for 4 bandpowers
+    vector<vector<float32_t>> channelBandPowers(CHANNELS, std::vector<float32_t>(BANDS));
+
+    // Array of channels where each channel has 4 elements for 4 bandpowers
+    vector<vector<float32_t>> channelRelativeBandPowers(CHANNELS, std::vector<float32_t>(BANDS));
+ 
+    // Fill the matrix channels with values
+    // for (size_t row = 0; row < rawData.size(); row++) {
+    //     for (size_t col = 1; col < rawData[row].size(); col++) {
+    //         allChannels.set_at(stof(rawData[row][col]), row, col);
+    //        std::cout << "Row: " << row << ", Column: " << col << ", Value: " << rawData[row][col] << std::endl;
+
+    //     }
+    // }
+
+    for (size_t i = 0; i < RAW_SAMPLE_NUMBER; i++) {
+        allChannels.set_at(inputSignal[i], i, 1);
+        std::cout << "Row: " << i << ", Column: " << 1 << ", Value: " << inputSignal[i] << std::endl; 
+    }
+    
+    // Note: Loop unrolling to help optimize and parallelize code
+    // Preprocessing steps 
+    for (size_t i = 0; i < CHANNELS; i=i+2) 
+    {
+         // Compute the singeSideFFT for all channels
+        channelFFTResults[i] = allChannels.singleSideFFT(i);
+        std::cout << "Print single-sided FFT results" <<  std::endl; 
+        channelFFTResults[i].prettyPrint();
+       // channelFFTResults[i+1] = allChannels.singleSideFFT(i+1);
+
+        // Compute the singleSidePower for all channels
+        channelPowerResults[i] = allChannels.singleSidePower(i);
+         std::cout << "Print single-sided  results" <<  std::endl; 
+        channelPowerResults[i].prettyPrint();
+        //channelPowerResults[i+1] = allChannels.singleSidePower(i+1);
+
+    }
+
+    // Computation of bandpowers
+    for (size_t i = 0; i < CHANNELS; i=i+2) 
+    {
+        // Bandpower for delta 
+        channelBandPowers[i][0] = channelPowerResults[i].singleSideBandPower(i, SAMPLE_FREQ, RAW_SAMPLE_NUMBER, 0);
+       // channelBandPowers[i+1][0] = channelPowerResults[i+1].singleSideBandPower(i+1, SAMPLE_FREQ, RAW_SAMPLE_NUMBER, 0);
+       
+        // Bandpower for theta 
+        channelBandPowers[i][1] = channelPowerResults[i].singleSideBandPower(i, SAMPLE_FREQ, RAW_SAMPLE_NUMBER, 1);
+       // channelBandPowers[i+1][1] = channelPowerResults[i+1].singleSideBandPower(i+1, SAMPLE_FREQ, RAW_SAMPLE_NUMBER, 1);
+
+        // Bandpower for alpha 
+        channelBandPowers[i][2] = channelPowerResults[i].singleSideBandPower(i, SAMPLE_FREQ, RAW_SAMPLE_NUMBER, 2);
+       // channelBandPowers[i+1][2] = channelPowerResults[i+1].singleSideBandPower(i+1, SAMPLE_FREQ, RAW_SAMPLE_NUMBER, 2);
+
+        // Bandpower for beta 
+        channelBandPowers[i][3] = channelPowerResults[i].singleSideBandPower(i, SAMPLE_FREQ, RAW_SAMPLE_NUMBER, 3);
+       // channelBandPowers[i+1][3] = channelPowerResults[i+1].singleSideBandPower(i+1, SAMPLE_FREQ, RAW_SAMPLE_NUMBER, 3);
+
+    }
+
+    // Computation of relative bandpowers
+    for (size_t i = 0; i < CHANNELS; i=i+2) 
+    {
+        // Each channel's power spectrum calculates the relative band powers of the 4 bands and stores them into 
+        // a 2D array. Outer index denotes the channel number, inner index denotes the band power value
+        channelRelativeBandPowers[i] = channelPowerResults[i].singleSideRelativeBandPower(channelBandPowers[i]);
+        //channelRelativeBandPowers[i+1] = channelPowerResults[i+1].singleSideRelativeBandPower(channelBandPowers[i+1]);
+
+    }
+
+
+    return 0;
+
+}
